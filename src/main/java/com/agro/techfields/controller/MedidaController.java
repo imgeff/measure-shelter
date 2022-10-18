08e1287bf75e0d0b3ac0743688e5b6850267caa4
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-10-18 10:25:53 -0300

    :sparkles: feat: Implementa endpoint GET /medida/{nomePlantacao}/{ilhaId}
    responsável por buscar todas as medidas de uma ilha

--- src/main/java/com/agro/techfields/controller/MedidaController.java
+++ src/main/java/com/agro/techfields/controller/MedidaController.java
@@ -1,11 +1,38 @@
 package com.agro.techfields.controller;
 
-public class MedidaController {
-  /**
-   * Atributos
-   */
+import com.agro.techfields.model.Medida;
+import com.agro.techfields.service.MedidaService;
 
-  /**
-   * Métodos
-   */
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.bson.types.ObjectId;
+
+
+@Path("/medida")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class MedidaController {
+  
+  @Inject
+  private MedidaService medidaService;
+
+  @GET
+  @Path("/{nomePlantacao}/{ilhaId}")
+  public Response buscarMedidas(
+      @PathParam("nomePlantacao") String nomePlantacao, 
+      @PathParam("ilhaId") ObjectId ilhaId
+  ) {
+    List<Medida> medidas = this.medidaService.buscarMedidas(nomePlantacao, ilhaId);
+    return Response.status(Status.OK).entity(medidas).build();
+  }
 }
