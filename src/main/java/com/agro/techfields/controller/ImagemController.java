68bea30fd55af5c0afffd26e6802cf26a65e10bb
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-10-18 10:11:04 -0300

    :sparkles: feat: Implementa endpoint GET /imagem
    responsável por baixar uma imagem

--- src/main/java/com/agro/techfields/controller/ImagemController.java
+++ src/main/java/com/agro/techfields/controller/ImagemController.java
@@ -1,11 +1,38 @@
 package com.agro.techfields.controller;
 
-public class ImagemController {
-  /**
-   * Atributos
-   */
+import com.agro.techfields.dto.ImagemDto;
+import com.agro.techfields.dto.PlantacaoImagemDto;
+import com.agro.techfields.model.Imagem;
+import com.agro.techfields.service.ImagemService;
 
-  /**
-   * Métodos
-   */
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.bson.types.ObjectId;
+
+
+@Path("/imagem")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ImagemController {
+  
+  @Inject
+  private ImagemService imagemService;
+
+  @GET
+  public Response baixarImagem(PlantacaoImagemDto plantacaoImagemDto) {
+    Imagem imagem = this.imagemService.baixarImagem(plantacaoImagemDto);
+    return Response.status(Status.OK).entity(imagem).build();
+  }
+  
 }
