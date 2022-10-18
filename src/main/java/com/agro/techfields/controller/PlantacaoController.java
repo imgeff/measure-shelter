aee7ea428ebeda8ba568944fe26afd2238c7c1e9
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-10-17 23:27:24 -0300

    :sparkles: feat: Implementa endpoint POST /plantacao
    responsável por fazer a criação de uma ilha no DB

--- src/main/java/com/agro/techfields/controller/PlantacaoController.java
+++ src/main/java/com/agro/techfields/controller/PlantacaoController.java
@@ -1,11 +1,29 @@
 package com.agro.techfields.controller;
 
-public class PlantacaoController {
-  /**
-   * Atributos
-   */
+import com.agro.techfields.dto.PlantacaoDto;
+import com.agro.techfields.model.Plantacao;
+import com.agro.techfields.service.PlantacaoService;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
-  /**
-   * Métodos
-   */
+
+@Path("/plantacao")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class PlantacaoController {
+
+  @Inject
+  PlantacaoService plantacaoService;
+
+  @POST
+  public Response criarPlantacao(PlantacaoDto plantacao) {
+    Plantacao plantacaoCriada = this.plantacaoService.criarPlantacao(plantacao);
+    return Response.status(Status.CREATED).entity(plantacaoCriada).build();
+  }
 }
