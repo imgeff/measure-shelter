1fd82a1428fabdbe0d833492a2c9239403c66771
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-10-17 23:38:34 -0300

    :sparkles: feat: Implementa endpoint GET /ilha/{plantacaoId}
    responsável por buscar todas as ilhas de uma plantacao

--- src/main/java/com/agro/techfields/controller/IlhaController.java
+++ src/main/java/com/agro/techfields/controller/IlhaController.java
@@ -1,11 +1,35 @@
 package com.agro.techfields.controller;
 
-public class IlhaController {
-  /**
-   * Atributos
-   */
+import com.agro.techfields.model.Ilha;
+import com.agro.techfields.service.IlhaService;
 
-  /**
-   * Métodos
-   */
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.bson.types.ObjectId;
+
+
+@Path("/ilha")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class IlhaController {
+
+  @Inject
+  private IlhaService ilhaService;
+
+  @GET
+  @Path("/{plantacaoId}")
+  public Response buscarIlhas(@PathParam("plantacaoId") ObjectId plantacaoId) {
+    List<Ilha> ilhas = this.ilhaService.buscarIlhas(plantacaoId);
+    return Response.status(Status.OK).entity(ilhas).build();
+  }
 }
