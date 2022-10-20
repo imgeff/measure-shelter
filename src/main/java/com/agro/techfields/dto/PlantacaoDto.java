05dcf307b03cc3220f0eca2497ab0165c7185029
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-10-20 07:57:42 -0300

    :white_check_mark: test: Adiciona teste do endpoint POST /plantacao

--- src/main/java/com/agro/techfields/dto/PlantacaoDto.java
+++ src/main/java/com/agro/techfields/dto/PlantacaoDto.java
@@ -7,6 +7,12 @@ public class PlantacaoDto {
   @Size(min = 3, message = "O campo nome deve ter mais de 3 caracteres")
   private String nome;
 
+  public PlantacaoDto() {}
+
+  public PlantacaoDto(String nome) {
+    this.nome = nome;
+  }
+
   public String getNome() {
     return nome;
   }
