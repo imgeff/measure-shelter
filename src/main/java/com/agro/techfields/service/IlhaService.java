59f1c417fb4f3cd511ef981a60cba6df80ca5473
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-10-21 23:50:17 -0300

    :recycle: refactor: Refatora service de Ilha
    Utiliza dos métodos de busca adicionados para facilitar o fluxo da operação

--- src/main/java/com/agro/techfields/service/IlhaService.java
+++ src/main/java/com/agro/techfields/service/IlhaService.java
@@ -42,13 +42,8 @@ public class IlhaService {
   }
 
   /** Busca ilhas. */
-  public List<Ilha> buscarIlhas(ObjectId idPlantacao) {
-
-    Plantacao plantacao = plantacaoRepository.findById(idPlantacao);
-
-    if (plantacao == null) {
-      throw new NotFoundException("Plantação");
-    }
+  public List<Ilha> buscarIlhas(ObjectId plantacaoId) {
+    Plantacao plantacao = this.plantacaoRepository.getPlantacao(plantacaoId);
 
     return plantacao.getIlhas();
   }
@@ -59,45 +54,27 @@ public class IlhaService {
     ObjectId ilhaId = atualizarIlhaDto.getIlhaId();
     float area = atualizarIlhaDto.getArea();
 
-    Plantacao plantacao = plantacaoRepository.findById(plantacaoId);
-
-    if (plantacao == null) {
-      throw new NotFoundException("Plantação");
-    }
-
+    Plantacao plantacao = this.plantacaoRepository.getPlantacao(plantacaoId);
     List<Ilha> ilhas = plantacao.getIlhas();
 
-    Ilha ilhaSelecionada = null;
-
-    for (int index = 0; index < ilhas.size(); index += 1) {
-      if (ilhas.get(index).getId().equals(ilhaId)) {
-        ilhaSelecionada = ilhas.get(index);
-        ilhaSelecionada.setArea(area);
-        ilhas.set(index, ilhaSelecionada);
-      }
-    }
-
+    Ilha ilha = this.plantacaoRepository.getIlha(plantacao, ilhaId);
+    ilha.setArea(area);
+    Integer indexIlha = ilhas.indexOf(ilha);
+    ilhas.set(indexIlha, ilha);
     plantacao.setIlhas(ilhas);
 
     plantacaoRepository.update(plantacao);
-    return ilhaSelecionada;
+    return ilha;
   }
 
   /** Deleta ilha. */
   public MensagemResult deletarIlha(String nomePlantacao, ObjectId ilhaId) {
-    Plantacao plantacao = plantacaoRepository.find("nome", nomePlantacao).firstResult();
-
-    if (plantacao == null) {
-      throw new NotFoundException("Plantação");
-    }
+    Plantacao plantacao = this.plantacaoRepository.getPlantacao(nomePlantacao);
 
     List<Ilha> ilhas = plantacao.getIlhas();
 
-    for (int index = 0; index < ilhas.size(); index += 1) {
-      if (ilhas.get(index).getId().equals(ilhaId)) {
-        ilhas.remove(index);
-      }
-    }
+    Ilha ilha = this.plantacaoRepository.getIlha(plantacao, ilhaId);
+    ilhas.remove(ilha);
 
     plantacao.setIlhas(ilhas);
     plantacaoRepository.update(plantacao);
